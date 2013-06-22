50ad11ff5cc50e0a4fe9accb8f0a07bee98d7ea6
Paul van der Meijs <paul@lowtone.nl>
2013-06-22 19:43:15 +0200

    Added support for Lowtone Media Types.

--- lowtone-media-dropbox.php
+++ lowtone-media-dropbox.php
@@ -18,7 +18,8 @@
 
 namespace lowtone\media\dropbox {
 
-	use lowtone\content\packages\Package;
+	use lowtone\content\packages\Package,
+		lowtone\media\types\Type;
 
 	// Includes
 	
@@ -26,9 +27,27 @@ namespace lowtone\media\dropbox {
 		return trigger_error("Lowtone Content plugin is required", E_USER_ERROR) && false;
 
 	$__i = Package::init(array(
-			Package::INIT_PACKAGES => array("lowtone"),
+			Package::INIT_PACKAGES => array("lowtone", "lowtone\\media"),
 			Package::INIT_MERGED_PATH => __NAMESPACE__,
 			Package::INIT_SUCCESS => function() {
+
+				\lowtone\media\addMediaType(new Type(array(
+						Type::PROPERTY_TITLE => __("Dropbox", "lowtone_media_dropbox"),
+						Type::PROPERTY_NEW_FILE_TEXT => __("Import a file from your Dropbox.", "lowtone_media_dropbox"),
+						Type::PROPERTY_SLUG => "dropbox",
+						Type::PROPERTY_IMAGE => plugins_url("/assets/images/dropbox-icon.png", __FILE__),
+						Type::PROPERTY_NEW_FILE_CALLBACK => function() {
+
+							echo '<div class="wrap">' . 
+								get_screen_icon() . 
+								'<h2>' . __("Select a file on Dropbox", "lowtone_media_dropbox") . '</h2>' . 
+								'<p>' . __("Use the chooser to select the file you want to import.", "lowtone_media_dropbox") . '</p>';
+
+							echo '</div>';
+
+								
+						}
+					)));
 
 				add_filter("media_upload_tabs", function($tabs) {
 					$tabs["dropbox"] = __("Dropbox", "lowtone_media_dropbox");
@@ -40,20 +59,20 @@ namespace lowtone\media\dropbox {
 					$appKey = get_option("dropbox_app_key");
 
 					echo sprintf('<script type="text/javascript" src="https://www.dropbox.com/static/api/1/dropbox.js" id="dropboxjs" data-app-key="%s"></script>', $appKey);
-				});
+				});*/
 
 				add_action("media_upload_dropbox", function() {
 					wp_enqueue_script("lowtone_dropbox", plugins_url("/assets/scripts/jquery.dropbox.js", __FILE__));
 
 					return wp_iframe(function() {
 						media_upload_header();
-
-						//echo '<iframe src="https://www.dropbox.com/chooser?origin=https%3A%2F%2Fapp.asana.com&amp;app_key=asana&amp;link_type=preview" style="display: block; width: 100%; height: 100%; background-color: white; border: none;"></iframe>';
-						//echo '<script type="text/javascript" src="https://www.dropbox.com/static/api/1/dropbox.js" id="dropboxjs" data-app-key="u2bmkj958mnne3y"></script>';
-						//echo '<script type="text/javascript">Dropbox.choose({iframe: true});</script>';
+						echo "FOO";
+						// echo '<iframe src="https://www.dropbox.com/chooser?origin=https%3A%2F%2Fapp.asana.com&amp;app_key=asana&amp;link_type=preview" style="display: block; width: 100%; height: 100%; background-color: white; border: none;"></iframe>';
+						// echo '<script type="text/javascript" src="https://www.dropbox.com/static/api/1/dropbox.js" id="dropboxjs" data-app-key="u2bmkj958mnne3y"></script>';
+						// echo '<script type="text/javascript">Dropbox.choose({iframe: true});</script>';
 					});
 
-				});*/
+				});
 
 				/*add_action("admin_enqueue_scripts", function() {
 					wp_enqueue_script("lowtone_media_dropbox", plugins_url("/assets/scripts/lowtone-media-dropbox.js", __FILE__), array("media-views"), false, true);
